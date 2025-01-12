fe5669debe5f1c93271118c964388b9b46c2ae4d
Bollyb <93448994+Bollyb@users.noreply.github.com>
2025-01-12 12:35:19 +0100

    CS5.py

--- CS5.py
+++ CS5.py
@@ -1,15 +1,26 @@
+'''
+Task # 05
+Network Packet Analyzer
+
+Develop a packet sniffer tool that captures and analyzes network packets. 
+Display relevant information such as source and destination IP addresses, protocols, 
+and payload data. Ensure the ethical use of the tool for educational purposes.
+
+'''
+
 from scapy.all import *
 from scapy.layers.inet import IP
 from scapy.layers import *
 
 def packet_analyzer(IP):
-
+# Check if packet is IPv4
     if packet.haslayer(IP):
+         # Get source and destination IP addresses
         source_ip = packet[IP].src
         destination_ip = packet[IP].dst
-
+# Get protocol
         protocol = packet[IP].proto
-
+ # Check if Raw layer exists
         if packet.haslayer(Raw):
             payload = packet[Raw].load
         else:
@@ -20,6 +31,6 @@ def packet_analyzer(IP):
         print(f"Protocol: {protocol}")
         print(f"Payload: {payload}")
         print("...................................")
-
+# Start sniffing
 sniff(filter="ip", prn=packet_analyzer)
         
